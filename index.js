d801cceefb2f6c722e868f0920b898bf79b88889
Anton Lapshin <antvallap@gmail.com>
2016-04-11 16:12:31 +0300

    New version

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-var instances=require("./gymdb/instances"),port=Number(process.env.PORT||8081);require("./server").start(port,instances.GYMTESTVK).then(function(){console.log("Listening port "+port)},function(e){throw e});
+var port=Number(process.env.PORT||8081);require("./server").start(port,{host:"ds013250.mlab.com",port:13250,database:"gymtest",username:"gymadmin",password:"24547294"}).then(function(){console.log("Listening port "+port)},function(r){throw r});
